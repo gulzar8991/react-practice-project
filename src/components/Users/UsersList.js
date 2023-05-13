e528b0c68aa924f5192747c20e29c195cac038e5
Md Gulzar <m.gulzar8991@gmail.com>
2023-05-13 10:19:46 +0530

    managing a list of users via state

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -6,11 +6,11 @@ const UserList = (props) => {
   return (
     <Card className={classes.users}>
       <ul>
-        {props.users.map((user) => {
-          <li>
+        {props.users.map((user) => (
+          <li key={user.id}>
             {user.name} ({user.age} years old)
-          </li>;
-        })}
+          </li>
+        ))}
       </ul>
     </Card>
   );
