fe37b891bee4947b26ed062a9b882ab7cb577a6e
Md Gulzar <m.gulzar8991@gmail.com>
2023-05-13 08:17:36 +0530

    added a user Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form>
+    <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
-      <input type="text" className="username" />
+      <input type="text" id="username" className="username" />
+      <label htmlFor="age">Age (Year)</label>
+      <input type="number" id="age" className="username" />
+      <button type="submit">Add User</button>
     </form>
   );
 };
